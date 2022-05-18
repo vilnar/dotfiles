0acf7fc98a8747318bae80b6cceac86b5e6cd0bb
vilnar <yura.gagarin9@gmail.com>
2022-05-18 09:48:13 +0300

    remove not used vars for command bookmarks

--- config/sublime-text/Packages/User/bookmarks.py
+++ config/sublime-text/Packages/User/bookmarks.py
@@ -1,30 +1,24 @@
 import sublime, sublime_plugin
-import os, re
+import os
 
 class PrintBookmarksCommand(sublime_plugin.TextCommand):
-    locations=[]
 
     def run(self, edit):
         items=[]
-        self.locations=[]
         all_files = list(filter(None, map(lambda f : f.file_name(), sublime.active_window().views())))
         common_prefix = os.path.commonprefix(all_files)
         for view in sublime.active_window().views():
-            prefix=""
+            prefix = ""
             if view.name():
-                prefix=view.name()+":"
+                prefix = view.name() + ":"
             elif view.file_name():
-                prefix=view.file_name()+":"
+                prefix = view.file_name() + ":"
                 if prefix.startswith(common_prefix):
                     prefix = prefix[len(common_prefix):]
             for region in view.get_regions("bookmarks"):
-                row,_=view.rowcol(region.a)
-                # line=re.sub('\s+', ' ', view.substr(view.line(region))).strip()
-                # items.append([prefix+str(row+1), line])
-                items.append(prefix+str(row+1))
-                self.locations.append((view, region))
+                row, _ = view.rowcol(region.a)
+                items.append(prefix + str(row + 1))
         if len(items) > 0:
-            print("debug  {}", items)
             win = self.view.window()
             win.new_file()
             view_temp = win.active_view()
