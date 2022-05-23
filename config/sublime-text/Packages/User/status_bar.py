08be40adff8be6d8f25d08c20087c928b59aac13
vilnar <yura.gagarin9@gmail.com>
2022-05-23 17:15:50 +0300

    fix names

--- config/sublime-text/Packages/User/status_bar.py
+++ config/sublime-text/Packages/User/status_bar.py
@@ -2,20 +2,21 @@ import sublime
 import sublime_plugin
 import os
 
+STATUS_KEY = "_view_status"
 
-class ShowFileStatusInStatusBar(sublime_plugin.EventListener):
+class ViewInStatusBar(sublime_plugin.EventListener):
     def on_activated_async(self, view):
         if view.is_scratch():
-            view.set_status('_file_status', "View: SCRATCH \U000026A1")
+            view.set_status(STATUS_KEY, "View: SCRATCH \U000026A1")
             return
         file_name = view.file_name()
         if not file_name:
-            view.set_status('_file_status', "View: TEMP \U0001F4DD")
+            view.set_status(STATUS_KEY, "View: TEMP \U0001F4DD")
             return
         if not os.path.exists(file_name):
-            view.set_status('_file_status', "File: NOT EXISTS \U0001F5D1")
+            view.set_status(STATUS_KEY, "File: NOT EXISTS \U0001F5D1")
             return
         if not os.access(file_name, os.W_OK):
-            view.set_status('_file_status', "File: READ ONLY \U0001F440")
+            view.set_status(STATUS_KEY, "File: READ ONLY \U0001F440")
             return
-        view.erase_status('_file_status')
+        view.erase_status(STATUS_KEY)
