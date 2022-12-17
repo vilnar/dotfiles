b4a20e926301503e6467af36c9b89a334b611f60
vilnar <yura.gagarin9@gmail.com>
2022-12-17 16:46:47 +0200

    remove plugins not used

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -29,12 +29,12 @@
 (add-hook 'after-make-frame-functions 'yr:new-frame-setup)
 
 
-;; (set-face-attribute 'default nil :family "Jetbrains Mono NL" :height 135)
-;; (set-face-attribute 'fixed-pitch nil :family "Jetbrains Mono NL")
-;; (set-face-attribute 'variable-pitch nil :family "Jetbrains Mono NL")
-(set-face-attribute 'default nil :family "Dejavu Sans Mono" :height 135)
-(set-face-attribute 'fixed-pitch nil :family "Dejavu Sans Mono")
-(set-face-attribute 'variable-pitch nil :family "Dejavu Sans Mono")
+(set-face-attribute 'default nil :family "Jetbrains Mono NL" :height 135)
+(set-face-attribute 'fixed-pitch nil :family "Jetbrains Mono NL")
+(set-face-attribute 'variable-pitch nil :family "Jetbrains Mono NL")
+;; (set-face-attribute 'default nil :family "Dejavu Sans Mono" :height 135)
+;; (set-face-attribute 'fixed-pitch nil :family "Dejavu Sans Mono")
+;; (set-face-attribute 'variable-pitch nil :family "Dejavu Sans Mono")
 
 
 ;; eol type
