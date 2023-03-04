8773081d0734f0e0aeca77755bbb2e0d216c50d8
vilnar <dwallin857@gmail.com>
2023-03-04 10:07:16 +0200

    add doom-modeline

--- emacs.d/ui.el
+++ emacs.d/ui.el
@@ -21,7 +21,7 @@
 (menu-bar-mode 1)
 (context-menu-mode 1)
 
-(scroll-bar-mode 1)
+(scroll-bar-mode -1)
 
 (set-face-attribute 'default nil :family "Jetbrains Mono NL" :height 135)
 (set-face-attribute 'fixed-pitch nil :family "Jetbrains Mono NL")
